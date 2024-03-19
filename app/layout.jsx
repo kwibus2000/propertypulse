ee2d2dd73a368c9d346d49ea86fab1ab559d6b2f
Ad Rienks <ad.rienks@gmail.com>
2024-03-19 21:39:35 +0100

    Add all

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties'
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+};
+export default MainLayout;
