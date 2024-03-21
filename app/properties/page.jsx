7868b02deed20ac5a04ec5119023f31b07ee53bb
Ad Rienks <ad.rienks@gmail.com>
2024-03-21 16:06:07 +0100

    Fetch Data From Server Components

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -17,6 +17,9 @@ async function fetchProperties() {
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
 
+  // Sort properties by date
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
